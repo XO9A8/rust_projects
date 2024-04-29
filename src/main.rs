cb337949df89e70529c78c61e77656a9478b2d50
XO9A8 <sabbir.hossain9912@gmail.com>
2024-04-29 23:05:41 +0600

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use first_game::visitor_info::{Visitor, VisitorAction};
 fn main() {
-    println!("Hello, world!");
+    let mut visitor_list: Vec<Visitor> = Vec::new();
+    visitor_list.push(Visitor::new("Bert", 45, VisitorAction::Accept));
+    visitor_list.push(Visitor::new("Steve", 15, VisitorAction::Refuse));
+    visitor_list.push(Visitor::new(
+        "Jon",
+        18,
+        VisitorAction::AcceptWithNote {
+            note: "Jon is a minor".to_string(),
+        },
+    ));
+    visitor_list.push(Visitor::new("Pete", 30, VisitorAction::Probation));
+    Visitor::process_visitor(&visitor_list, &take_name());
+}
+
+fn take_name() -> String {
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    name.trim().to_string().to_lowercase()
 }
